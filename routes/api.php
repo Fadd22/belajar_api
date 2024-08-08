e82ec0483c6f9dd09d5c66047f55fc8d28898f1d
Fadd22 <moch.fadelilham9@gmail.com>
2024-08-08 10:50:45 +0700

    Belajar

--- routes/api.php
+++ routes/api.php
@@ -31,12 +31,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::resource('film', FilmController::class);
 
-Route::middleware(['auth:sanctum'])->group(function () {
+// Route::middleware(['auth:sanctum'])->group(function () {
 Route::post('logout', [AuthController::class, 'logout']);
 Route::resource('kategori', KategoriController::class);
 Route::resource('genre', GenreController::class);
 Route::resource('actor', ActorController::class);
-});
+// });
 
 //auth route
 Route::post('login',[AuthController::class, 'login']);
